8661823b61550a8bdf456bb7995ae394deee9b45
Vincent Hung <vincent.hung21@ycdsbk12.ca>
2020-11-27 15:08:21 +0000

    zipzap method skeleton finished

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -3,7 +3,7 @@ import gr11review.part2.Utility.*;
 public class Main {
   public static void main(String[] args) {
       String str;
-      str = Utility.zipZap("zipzap");
-      //System.out.println(str);
+      str = Utility.zipZap("zzipC:Dzop");
+      System.out.println(str);
     }
 }
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -7,12 +7,11 @@ public class Utility{
       int intcounter;
       String stranswer;
       stranswer = "";
-      for (intcounter = 0 ; intcounter < intlength - 1 ; intcounter++) {
+      for (intcounter = 0 ; intcounter < intlength ; intcounter++) {
         stranswer = stranswer + str.substring(intcounter, intcounter + 1);
-        System.out.println(stranswer);
         if (intcounter > 0) {
           if (str.charAt(intcounter - 1) == 'z' && str.charAt(intcounter + 1) == 'p')
-            stranswer = stranswer.substring(0 , stranswer.length());
+            stranswer = stranswer.substring(0 , stranswer.length()-1);
           }
       }
       return stranswer;
