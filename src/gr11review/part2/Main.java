70d64245576a71a8b3e63612259c72877253e328
Vincent Hung <vincent.hung21@ycdsbk12.ca>
2020-11-27 15:05:22 +0000

    debugging zipzap

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -1,10 +1,9 @@
 package gr11review.part2;
-import gr11review.part2.*;
-
+import gr11review.part2.Utility.*;
 public class Main {
   public static void main(String[] args) {
       String str;
       str = Utility.zipZap("zipzap");
-      System.out.println(str);
+      //System.out.println(str);
     }
 }
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -7,10 +7,13 @@ public class Utility{
       int intcounter;
       String stranswer;
       stranswer = "";
-      for (intcounter = 0 ; intcounter < intlength ; intcounter++) {
+      for (intcounter = 0 ; intcounter < intlength - 1 ; intcounter++) {
         stranswer = stranswer + str.substring(intcounter, intcounter + 1);
-        if (str.charAt(intcounter) == 'z' && str.charAt(intcounter + 2) == 'p')
-          stranswer = stranswer.substring(0 , stranswer.length()-1);
+        System.out.println(stranswer);
+        if (intcounter > 0) {
+          if (str.charAt(intcounter - 1) == 'z' && str.charAt(intcounter + 1) == 'p')
+            stranswer = stranswer.substring(0 , stranswer.length());
+          }
       }
       return stranswer;
     }
