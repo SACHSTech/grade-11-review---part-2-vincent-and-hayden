d19e9a772b3b69619c9354add039a4a447cbd05b
repl.it user <replituser@example.com>
2020-11-27 14:30:18 +0000

    working on zipzap

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -1,2 +1,10 @@
 package gr11review.part2;
+import gr11review.part2.*;
 
+public class Main {
+  public static void main(String[] args) {
+      String str;
+      str = Utility.zipZap("zipzap");
+      System.out.println(str);
+    }
+}
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,5 +1,18 @@
 package gr11review.part2;
 
-public class Utility {
-    
+public class Utility{
+    public static String zipZap(String str) {
+      int intlength;
+      intlength = str.length();
+      int intcounter;
+      String stranswer;
+      stranswer = "";
+      for (intcounter = 0 ; intcounter < intlength ; intcounter++) {
+        stranswer = stranswer + str.substring(intcounter, intcounter + 1);
+        if (str.charAt(intcounter) == 'z' && str.charAt(intcounter + 2) == 'p')
+          stranswer = stranswer.substring(0 , stranswer.length()-1);
+      }
+      return stranswer;
+    }
 }
+
