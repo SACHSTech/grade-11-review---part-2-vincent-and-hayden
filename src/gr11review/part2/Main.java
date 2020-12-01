296bd962565f769457f80b32ed458f3869d3b279
haydenchung03 <hayden.chung21@ycdsbk12.ca>
2020-12-01 04:40:24 +0000

    Merged to main and changed my branch

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -65,31 +65,5 @@ public class Main {
       int n = Integer.parseInt(keyboard.readLine());
       Utility.diagonal(n);
     }
-    /**
-    * Testing of methods
-    * @author: Vincent Hung
-    */  
-    
-    //Testing ZipZap
-    String str;
-    str = Utility.zipZap("zzipC:Dzop");
-    System.out.println(str);
-    //Testing longestWord
-    System.out.println(Utility.longestWord("/home/runner/grade-11-review-part-2-vincent-and-hayden/src/gr11review/part2/words.txt"));
-    
-    //Testing tenRun
-    int[] nums = {2,10,3,4,20};
-    int[] intanswers = (Utility.tenRun(nums));
-    System.out.println(Arrays.toString(intanswers));
-
-    //Testing linearIn
-    boolean test;
-    int[] outer = {1,2,4,6};
-    int[] inner = {2,3,4};
-    test = Utility.linearIn(outer, inner);
-    System.out.println(test);
-    
-    //Testing pascalTri
-    Utility.pascalTri(7, 7);
   }
 }
