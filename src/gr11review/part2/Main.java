459051c58edbd6c6785cc6307a7724fc9f0b8b5c
haydenchung03 <hayden.chung21@ycdsbk12.ca>
2020-12-01 03:22:16 +0000

    Adding comments and error fixes

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -5,33 +5,33 @@ import gr11review.part2.Utility.*;
 import java.util.*;
 import java.util.Arrays;
 
-public class Main{
-  public static void main(String[] args) throws IOException{
+public class Main {
+  public static void main(String[] args) throws IOException {
     BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
     String filenametxt = "src/gr11review/part2/words.txt";
     System.out.println("Which Program do you want to run");
     String strOption = keyboard.readLine();
-    if(strOption.equals("Method 2")){
+    if (strOption.equals("Method 2")) {
       String str;
       int intInput;
       System.out.println("Enter any sentence");
       str = keyboard.readLine();
       intInput = Utility.sumNumbers(str);
       System.out.println(intInput);
-    }else if(strOption.equals("File IO 2")){
+    } else if(strOption.equals("File IO 2")) {
       String strFinalName = Utility.alphaWord(filenametxt);
       System.out.println("First word based on alphabetical order: "+strFinalName);
-    }else if(strOption.equals("Array 2")){
+    } else if(strOption.equals("Array 2")) {
       int[] nums = {1, 0, 2, 1, 2};
       System.out.println("Enter number to change");
       int Value = Integer.parseInt(keyboard.readLine());
       Utility.notAlone(nums, Value);
       System.out.println("The Changed Array is: "+Arrays.toString(nums));
-    }else if(strOption.equals("Array 4")){
-      int[] nums = {10,10};
+    } else if(strOption.equals("Array 4")) {
+      int[] nums = {10, 10};
       boolean Condition = Utility.canBalance(nums);   
       System.out.println(Condition);  
-    }else if(strOption.equals("Array 6")){
+    } else if(strOption.equals("Array 6")) {
       System.out.println("Pick a number");
       int n = Integer.parseInt(keyboard.readLine());
       Utility.diagonal(n);
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -5,97 +5,112 @@ import java.util.*;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 
-public class Utility{
-  public static int sumNumbers(String str){
+// Method 2: Add numbers together
+public class Utility {
+  public static int sumNumbers(String str) {
+    // Setting Variables
     int intLength;
     intLength = str.length();
     int intCount;
     int intSum = 0;
     String strWord = "";
-    for(intCount = 0; intCount < intLength; intCount++){
-      if(Character.isDigit(str.charAt(intCount))) {
-        if (intCount < intLength-1 && Character.isDigit(str.charAt(intCount+1))){
+    // Looping until every character is check through
+    for (intCount = 0; intCount < intLength; intCount++) {
+      // See if the value is a digit
+      if (Character.isDigit(str.charAt(intCount))) {
+        // Conditions to see if you can add a number in the string
+        if (intCount < intLength-1 && Character.isDigit(str.charAt(intCount+1))) {
+           // Finding a number within the sentence
           strWord += str.charAt(intCount);
-        }else{
+        } else {
+          // Adding the numbers together
           strWord += str.charAt(intCount);
           intSum += Integer.parseInt(strWord);
           strWord = "";
         }
       }
     }
+    // return sum
     return intSum;
   }
-
-  public static String alphaWord(String filenametxt)throws IOException{
+// File IO 2: Reading words in file and picking the first alphabetically first word
+  public static String alphaWord (String filenametxt) throws IOException {
     BufferedReader FileText = new BufferedReader(new FileReader(filenametxt));
+    // Setting Variables and Reading File
     String strWord;
     String strFinalWord;
     int intCompare;
     strWord = FileText.readLine();
     strFinalWord = FileText.readLine();
-    while(strWord != null){
+    // As long as it is not in the end of the file, continue reading it. 
+    while (strWord != null) {
+      // Compares the words in the text file
       intCompare = strFinalWord.compareToIgnoreCase(strWord);
-      if(intCompare > 0){
+      if (intCompare > 0) {
         strFinalWord = strWord;
       }
       strWord = FileText.readLine();
       }
+    // Closes file and returns the word that is alphabetically first
     FileText.close();
     return strFinalWord;
   }
-  public static int[] notAlone(int[] nums, int value)throws IOException{
+  // Array 2: Print out numbers
+  public static int[] notAlone(int[] nums, int value) throws IOException {
     int intCount = 0;
-    while(intCount < nums.length){
+    while (intCount < nums.length) {
       intCount++;
-      if(intCount > 0 && intCount < nums.length - 1 && nums[intCount] == value){
-      if(nums[intCount] != nums[intCount-1] && nums[intCount] !=nums[intCount+1])
-        nums[intCount] = Math.max(nums[intCount-1], nums[intCount+1]);
+      if (intCount > 0 && intCount < nums.length - 1 && nums[intCount] == value) {
+        if(nums[intCount] != nums[intCount-1] && nums[intCount] !=nums[intCount+1])
+          nums[intCount] = Math.max(nums[intCount-1], nums[intCount+1]);
       }
     }
     return nums;
   }
-  public static boolean canBalance(int[] nums)throws IOException{
+  public static boolean canBalance(int[] nums) throws IOException {
     int intLeftSum = 0;
     int intCount = 0;
-    while(intCount < nums.length){
+    while (intCount < nums.length) {
       intLeftSum += nums[intCount];
       intCount++;
       int intRightSum = 0;
       int intCount2 = nums.length-1;
-      while(intCount2 > intCount){
+      while (intCount2 > intCount) {
         intCount2--;
         intRightSum += nums[intCount2];
       }
-      if(intRightSum == intLeftSum){
+      if (intRightSum == intLeftSum) {
         return false;
       }
     }
     return true;
   }
-  public static void diagonal(int n)throws IOException{
+  public static void diagonal(int n) throws IOException {
     int intCount = 0;
     int intCount1 = 0;
     int[][] intArray;
     intArray = new int[n][n];
-    try{
+    try {
     PrintWriter theWriter = new PrintWriter(new FileWriter("src/gr11review/part2/diagonalOut.txt"));
-    for(intCount = 0; intCount < n; intCount++){
-      for(intCount1 = 0; intCount1 < n; intCount1++){
-        //intArray[intCount][intCount1] = 0;
-        if(intCount1 == (n - intCount - 1)){
-          intArray[intCount][intCount1] = 1;
-        }else if(intCount1 > (n - intCount - 1)){
-          intArray[intCount][intCount1] = 2;
-       }else if(intCount1 < n - intCount - 1){
+    for (intCount = 0; intCount < n; intCount++) {
+      for (intCount1 = 0; intCount1 < n; intCount1++) {
+        if (intCount1 < (n - intCount - 1)) {
           intArray[intCount][intCount1] = 0;
+        } else if(intCount1 == (n - intCount - 1)) {
+          intArray[intCount][intCount1] = 1;
+        } else if(intCount1 > n - intCount - 1) {
+          intArray[intCount][intCount1] = 2;
         }
-        
-        theWriter.print(intArray[intCount][intCount1]);
       }
     }
+    for (intCount = 0; intCount < n; intCount++) {
+      for (intCount1 = 0; intCount1 < n; intCount1++) {
+        theWriter.print(intArray[intCount][intCount1]);
+      }
+      theWriter.println("");
+    }
     theWriter.close();
-    }catch(IOException e){
-      System.out.println("Error");
+    } catch(IOException Hayden) {
     }
   }
 }
