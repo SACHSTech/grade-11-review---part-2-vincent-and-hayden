bc01f4b4f8aff54e03ecf1f104cb77eef527f09f
haydenchung03 <hayden.chung21@ycdsbk12.ca>
2020-12-01 03:51:43 +0000

    Added user input and Comments

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -8,9 +8,11 @@ import java.util.Arrays;
 public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+    // Variables
     String filenametxt = "src/gr11review/part2/words.txt";
     System.out.println("Which Program do you want to run");
     String strOption = keyboard.readLine();
+    // Method 2
     if (strOption.equals("Method 2")) {
       String str;
       int intInput;
@@ -18,15 +20,18 @@ public class Main {
       str = keyboard.readLine();
       intInput = Utility.sumNumbers(str);
       System.out.println(intInput);
+    // File IO 2
     } else if(strOption.equals("File IO 2")) {
       String strFinalName = Utility.alphaWord(filenametxt);
       System.out.println("First word based on alphabetical order: "+strFinalName);
+    // Array 2
     } else if(strOption.equals("Array 2")) {
+      // Getting the number of numbers within Array
       System.out.println("Amount of numbers in Array");
       int intOption2 = Integer.parseInt(keyboard.readLine());
       int[] nums = new int[intOption2];
       int intCount = 0;
-      while(intCount < intOption2){
+      while (intCount < intOption2) {
         System.out.println("Enter the number: ");
         nums[intCount] = Integer.parseInt(keyboard.readLine());
         intCount++;
@@ -35,11 +40,21 @@ public class Main {
       int Value = Integer.parseInt(keyboard.readLine());
       Utility.notAlone(nums, Value);
       System.out.println("The Changed Array is: " + Arrays.toString(nums));
+    // Array 4
     } else if(strOption.equals("Array 4")) {
-      
-      int[] nums = {10, 10};
+      // Getting the number of numbers within Array
+      System.out.println("Amount of numbers in Array");
+      int intOption2 = Integer.parseInt(keyboard.readLine());
+      int[] nums = new int[intOption2];
+      int intCount = 0;
+      while (intCount < intOption2) {
+        System.out.println("Enter the number: ");
+        nums[intCount] = Integer.parseInt(keyboard.readLine());
+        intCount++;
+      }
       boolean Condition = Utility.canBalance(nums);   
       System.out.println(Condition);  
+    // Array 6
     } else if(strOption.equals("Array 6")) {
       System.out.println("Pick a number");
       int n = Integer.parseInt(keyboard.readLine());
